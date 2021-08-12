f656bc18b18f3bc10c22d58756866c83443c56cd
chaips <ar0439708@gmail.com>
2021-08-12 11:28:17 -0500

    Added code for biometric authentication

--- app/src/main/java/com/example/biometricauthentication/MainActivity.kt
+++ app/src/main/java/com/example/biometricauthentication/MainActivity.kt
@@ -1,11 +1,86 @@
 package com.example.biometricauthentication
 
+import android.annotation.SuppressLint
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.biometricauthentication.databinding.ActivityMainBinding
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+    private val TAG = "MESSAGE"
+
+    private var cancellationSignal:CancellationSignal? = null
+    val authenticationCallback:BiometricPrompt.AuthenticationCallback
+    get() = @RequiresApi(Build.VERSION_CODES.P)
+    object : BiometricPrompt.AuthenticationCallback() {
+        override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+            super.onAuthenticationError(errorCode, errString)
+            notifyUser("Authentication error: $errString")
+        }
+
+        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+            super.onAuthenticationSucceeded(result)
+            notifyUser("Authentication Success!")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        biometricSupport()
+        /*val biometricPrompt : BiometricPrompt = BiometricPrompt.Builder(this)
+            .setTitle("Title")
+            .setSubtitle("Authenticaion is required")
+            .setDescription("Fingerprint Authentication")
+            .setNegativeButton("Cancel", this.mainExecutor, DialogInterface.OnClickListener { dialog, which ->
+            }).build()
+        biometricPrompt.authenticate(getCancellationSignal(), mainExecutor, authenticationCallback)*/
+    }
+
+    private fun notifyUser(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun getCancellationSignal():CancellationSignal{
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            Log.i(TAG, "Authentication was cancelled by the user")
+        }
+        return cancellationSignal as CancellationSignal
+    }
+
+    private fun biometricSupport():Boolean{
+        val keyguardManager:KeyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as
+                KeyguardManager
+        if (!keyguardManager.isKeyguardSecure) {
+            notifyUser("Fingerprint hs not been enabled in settings.")
+            return false
+        }
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED) {
+            notifyUser("Fingerprint hs not been enabled in settings.")
+            return false
+        }
+        return if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT))
+            true
+        else
+            true
     }
 }
